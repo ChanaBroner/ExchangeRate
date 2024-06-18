6ef7d04b481c6e5a48a9e5cfd06ee2d08d04d42b
ChanaBroner <125824958+ChanaBroner@users.noreply.github.com>
2024-06-18 09:16:03 +0300

    Add files via upload

---
+++ ExchangeRateApi/Models/ExchangeRateResponse.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace ExchangeRatesApi.Models
+{
+    public class ExchangeRateResponse
+    {
+        public ExchangeRateResponse()
+        {
+            ConversionRates = new Dictionary<string, double>();
+        }
+
+        [JsonProperty("conversion_rates")]
+        public Dictionary<string, double> ConversionRates { get; set; }
+    }
+}
